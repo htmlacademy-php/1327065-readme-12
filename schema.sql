3c7470dc39c0e41494e98f681ea67913acc0071c
Darvetra <darvetra13@gmail.com>
2020-10-20 22:24:38 +0300

    исправление ошибок по запросам к БД

--- schema.sql
+++ schema.sql
@@ -1,73 +1,79 @@
-DROP DATABASE readme -- только для обучения
+DROP DATABASE IF EXISTS readme; -- только для обучения
 CREATE DATABASE readme
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_cli
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+USE readme;
 
-USE readme
-
-CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE, -- ауто инкремент автоматом гененрирует айди
-  timestamp_add TIMESTAMP,
-  email VARCHAR(128) NOT NULL UNIQUE,
-  login VARCHAR(128) NOT NULL UNIQUE,
-  password CHAR(64) NOT NULL,
-  avatar_path CHAR,
-  INDEX (login)
+CREATE TABLE users
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY UNIQUE, -- ауто инкремент автоматом гененрирует айди
+    timestamp_add TIMESTAMP,
+    email         VARCHAR(128) NOT NULL UNIQUE,
+    login         VARCHAR(64)  NOT NULL UNIQUE,
+    password      VARCHAR(64)  NOT NULL,
+    avatar_path   VARCHAR(1024),
+    INDEX (login)
 );
 
-CREATE TABLE posts (
-  post_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  timestamp_add TIMESTAMP,
-  title CHAR NOT NULL,
-  content VARCHAR,
-  quote_author CHAR,
-  image_path CHAR,
-  video_path CHAR,
-  link CHAR,
-  view_count INT,
-  author_id INT NOT NULL,
-  content_type CHAR,
-  hashtag CHAR,
-  INDEX (title, content)
+CREATE TABLE posts
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    timestamp_add TIMESTAMP,
+    title         VARCHAR(128) NOT NULL,
+    content       VARCHAR(1024),
+    quote_author  VARCHAR(1024),
+    image_path    VARCHAR(1024),
+    video_path    VARCHAR(1024),
+    link          VARCHAR(1024),
+    view_count    INT,
+    author_id     INT  NOT NULL,
+    content_type  VARCHAR(1024),
+    hashtag       VARCHAR(1024),
+    INDEX (title)
 );
 
-CREATE TABLE comments (
-  comment_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  timestamp_add TIMESTAMP,
-  post_id INT NOT NULL,
-  author_id INT NOT NULL,
-  comment VARCHAR,
-  commentator_id INT NOT NULL -- в ТЗ почему-то отсутствует данное поле/связь
+CREATE TABLE comments
+(
+    id             INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    timestamp_add  TIMESTAMP,
+    post_id        INT NOT NULL,
+    commentator_id INT NOT NULL,
+    comment        VARCHAR(1024)
 );
 
-CREATE TABLE likes (
-  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  user_id INT NOT NULL,
-  post_id INT NOT NULL
+CREATE TABLE likes
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL
 );
 
-CREATE TABLE subscription (
-  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  follower_id INT NOT NULL,
-  influencer_id INT NOT NULL
+CREATE TABLE subscription
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    follower_id   INT NOT NULL,
+    influencer_id INT NOT NULL
 );
 
-CREATE TABLE messages (
-  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  timestamp_add TIMESTAMP,
-  content VARCHAR,
-  sender_id INT NOT NULL,
-  recipient_id INT NOT NULL
+CREATE TABLE messages
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    timestamp_add TIMESTAMP,
+    content       VARCHAR(1024),
+    sender_id     INT NOT NULL,
+    recipient_id  INT NOT NULL
 );
 
-CREATE TABLE hashtags (
-  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  hashtag CHAR,
-  INDEX (hashtag)
+CREATE TABLE hashtags
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    hashtag CHAR,
+    INDEX (hashtag)
 );
 
-CREATE TABLE content_type (
-  id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-  type CHAR,
-  icon CHAR
+CREATE TABLE content_type
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    type VARCHAR(1024),
+    icon VARCHAR(1024)
 );
